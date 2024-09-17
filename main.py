f7793ac8288417e74d9da9df0763f896b9fb1ab5
Joyson Gaurea <jng05929@gmail.com>
2024-09-16 22:58:28 -0400

    search function implement ENPM611

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import read_csv
 import json_converter
 import search
 import json
+import pandas as pd
 
 def main():
     csv_file_path = 'data.csv'
@@ -17,15 +18,24 @@ def main():
     json_converter.save_json_to_file(json_data, json_file_path)
     
     # Print JSON data
-    print("JSON Data:\n", json_data)
+    # print("JSON Data:\n", json_data)
     
     # uncomment the search_string and search_results once you implement search module
                   # Step 4: Search in JSON
-    # search_string = input("Enter a string to search in the JSON data: ")
-    # search_results = search.search_json(json.loads(json_data), search_string)
+    search_string = input("Enter a string to search in the JSON data: ")
+    search_results = search.search_json(json.loads(json_data), search_string)
     
                   # Print search results
     # print("Search Results:\n", json.dumps(search_results, indent=4))
+    
+    print("\nSearch Results:\n")
+    data_frame = pd.DataFrame(columns=search_results[0].keys())
+    
+    for i in range(len(search_results)):
+        data_frame.loc[i] = search_results[i].values()
+    
+    data_frame = data_frame.set_index('User')
+    print(data_frame.to_markdown())
 
 if __name__ == "__main__":
     main()
--- search.py
+++ search.py
@@ -1,8 +1,24 @@
-import json
+import re
+from fuzzywuzzy import fuzz
 
 def search_json(json_data, search_string):
     results = []
     # Place your search code here
     # you will have to loop through the json_data file you created earlier
     # finally you can store the match in the result list and return it
+    
+    keywords = search_string.split()
+    
+    for item in json_data:
+        temp = {}
+        for key, value in item.items():
+            if key == "User":
+                temp[key] = value
+            for keyword in keywords:
+                if re.search(keyword, key, re.IGNORECASE) or re.search(keyword, str(value), re.IGNORECASE):
+                    temp[key] = value
+                elif fuzz.partial_ratio(keyword.lower(), key.lower()) > 80 or fuzz.partial_ratio(keyword.lower(), str(value).lower()) > 80:
+                    temp[key] = value
+        results.append(temp)
+    
     return results
